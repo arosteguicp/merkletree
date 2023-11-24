ba6e0ed7d22d28169d71ebb478f6770cf9ec8f4f
camila <129806557+arosteguicp@users.noreply.github.com>
2023-11-23 23:46:43 -0500

    Update main.cpp
    merk

--- main.cpp
+++ main.cpp
@@ -1,16 +1,35 @@
-#include <iostream>
-#include "MerkleNode.h"
+
+#include "Merkle_Tree.h"
+
 int main() {
     std::vector<std::string> data_blocks = {"block1", "block2", "block3", "block4"};
     MerkleTree merkle_tree(data_blocks);
 
-    auto result = merkle_tree.verify_data("block1");
-    std::cout << "Data is valid: " << std::boolalpha << result.first << std::endl;
-    std::cout << "Proof: [";
-    for (const auto& step : result.second) {
-        std::cout << "('" << step.first << "', '" << step.second << "'), ";
+    std::vector<std::string> data_to_verify = {"block1", "block2", "block5", "block3"};
+
+    for (const auto& data : data_to_verify) {
+        std::vector<std::pair<std::string, std::string>> proof;
+        bool isValid = merkle_tree.verifyData(data, proof);
+
+        std::cout << "\n================== Merkle Tree Verification ==================\n";
+        std::cout << "Data Validation Result for \"" << data << "\": " << (isValid ? "SUCCESSFUL" : "FAILED") << "\n\n";
+        if (isValid) {
+            std::cout << "Merkle Proof Path (from leaf to root) for \"" << data << "\":\n";
+            for (size_t i = 0; i < proof.size(); ++i) {
+                std::cout << "  Step " << (i + 1) << ":\n";
+                std::cout << "    -> Direction: " << proof[i].first << "\n";
+                std::cout << "    -> Hash:      " << proof[i].second << "\n";
+                std::cout << "    ------------------------------------------------\n";
+            }
+        } else {
+            std::cout << "The specified data \"" << data << "\" was not found in the Merkle Tree.\n";
+        }
+        std::cout << "================================================================\n";
     }
-    std::cout << "]" << std::endl;
+    std::cout<<"arbol"<<std::endl;
+    printTree(merkle_tree.get_root());
+    std::cout<<"leaves "<<std::endl;
+    printLeaf(merkle_tree.get_leaves());
 
     return 0;
 }
