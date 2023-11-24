bf168e903bb772aa80ddbcf5c56be196b0b2fb66
camila <129806557+arosteguicp@users.noreply.github.com>
2023-11-23 23:46:04 -0500

    Update myhash.h

--- myhash.h
+++ myhash.h
@@ -1,48 +1,17 @@
 #include <iostream>
 #include <vector>
-#include <chrono>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <iomanip>
 
-
-// Inicializar la semilla solo una vez al comienzo del programa
-void inicializarSemilla() {
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-}
-
-int cual() {
-    // No cambies la semilla aquí, ya que se inicializa solo una vez al comienzo del programa
-    int i = std::rand() % 30;
-    return (i % 2 == 0) ? -1 : 0;
-}
-
-std::string f_hash(std::string data) {
-    // No cambies la semilla aquí, ya que se inicializa solo una vez al comienzo del programa
-    std::string result = "          ";
-    std::vector<int> azar;
-
-    // Hash tendrá siempre 10 de tamaño
-    for (int i = 0; i < 4; i++) {
-        int j = cual();
-        if (j == 0) {
-            result[i] = data[j];
-            result[2 * i + 1] = data[data.length() - 1];
-        } else {
-            result[i] = data[j + std::rand() % (data.length() - 1)];
-            result[2 * i + 1] = data[j + 1];
-        }
-
-        azar.push_back(std::rand() % 20);
+//hash unico
+std::string customHash(const std::string& input) {
+    unsigned long hash = 5381;
+    for (char c : input) {
+        hash = ((hash << 5) + hash) + static_cast<int>(c);
     }
-
-    result[4] = static_cast<char>('a' + azar[0]);
-    result[6] = static_cast<char>('a' + azar[1]);
-    result[8] = static_cast<char>('a' + azar[2]);
-    result[9] = static_cast<char>('a' + azar[3]);
-
-    for (auto i : azar) {
-        std::cout << i << " ";
-    }
-
-    azar.clear();
-    return result;
+    std::stringstream ss;
+    ss << std::hex << hash;
+    return ss.str();
 }
-
